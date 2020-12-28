115ed7ac8ef23c63eff4f6d252148ba4d5ddbf90
Syed Shah Safiulla Alvi <syed1998.ec@gmail.com>
2020-12-28 17:25:38 +0530

    Python code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Biiling-of-a-retail-store
-A python program prompting the user to enter the price and ask for quantity. A revelant discount is applied based on some criterias. Atlast a structured bill is printed.
+'''A python program prompting the user to enter the price and ask for quantity. A revelant discount is applied based on some criterias. Atlast a structured bill is printed.'''
---
+++ README.py
@@ -0,0 +1,29 @@
+# Biiling-of-a-retail-store
+'''A python program prompting the user to enter the price and ask for quantity. A revelant discount is applied based on some criterias. Atlast a structured bill is printed.'''
+
+print("\t***ABC Retail Store***")
+items=int(input("Enter the numner of items purchased: "))
+price=float(input("Enter the total amount: "))
+
+print("\n\nItems purchased: \t{}\nTotal amount:\t\t{}\n".format(items,price))
+
+if price>100 and price<199.99:
+    print("Yay! Discount of 10% is applied.")
+    discount=10/100*price
+
+elif price>200 and price<299.99:
+    print("Yay! Discount of 15% is applied.")
+    discount=15/100*price
+
+elif price>300 and price<499.99:
+    print("Yay! Discount of 20% is applied.")
+    discount=20/100*price
+
+elif price>500:
+    print("Yay! Discount of 25% is applied.")
+    discount=25/100*price
+    
+finalAmount=price-discount
+
+print("Discount amount:\t{}\n----------------------------------\nFinal Amount:\t\t{}\n---------------------------------".format(discount,finalAmount))
+print("--Thank you!! Visit again--")
