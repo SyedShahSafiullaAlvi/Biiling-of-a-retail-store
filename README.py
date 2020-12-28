dc82b43a99593aa8f30b2531abf2a5aa369a635a
Syed Shah Safiulla Alvi <syed1998.ec@gmail.com>
2020-12-28 17:32:51 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
-# Biiling-of-a-retail-store
+# Billing-of-a-retail-store
 '''A python program prompting the user to enter the price and ask for quantity. A revelant discount is applied based on some criterias. Atlast a structured bill is printed.'''
 
 print("\t***ABC Retail Store***")
